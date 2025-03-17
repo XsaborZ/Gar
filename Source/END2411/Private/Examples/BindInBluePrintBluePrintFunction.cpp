062c7236094a84c508126a9788b8780563528d4f
Joseph Horsch <132273715+XsaborZ@users.noreply.github.com>
2025-03-17 14:34:09 -0400

    week2 part 2 Lecture Finished

---
+++ Source/END2411/Private/Examples/BindInBluePrintBluePrintFunction.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Examples/BindInBluePrintBluePrintFunction.h"
+
+// Add default functionality here for any IBindInBluePrintBluePrintFunction functions that are not pure virtual.
